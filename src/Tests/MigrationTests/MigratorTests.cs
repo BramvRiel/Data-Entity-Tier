0d8638989a244b731828e49ec5a0500cdb8e24b8
Bram van Riel <bram.vanriel@ogd.nl>
2016-12-22 23:48:54 +0100

    Added unit test project for migrator calls.

---
+++ src/Tests/MigrationTests/MigratorTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DataEntityTier;
+using System.IO;
+
+namespace MigrationTests
+{
+    [TestClass]
+    public class MigratorTest
+    {
+        [TestMethod]
+        public void MigratorBlank()
+        {
+            NHibernateSessionFactory.Migrate(new MigrationFileWriter());
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(DirectoryNotFoundException))]
+        public void MigratorNotExistingDirectory()
+        {
+            NHibernateSessionFactory.Migrate(new MigrationFileWriter(directory: "DoesntExists"));
+        }
+    }
+}
